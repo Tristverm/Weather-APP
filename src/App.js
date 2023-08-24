8da5550736299e09834cdd015f0de0a0a59dfc08
Tristverm <emmanuelkyalo72@gmail.com>
2023-08-24 22:52:29 +0300

    Addin React icons and importing relevant weather icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
-import React {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+
+// importing axios for comm
+
+import axios from "axios";
+
+//importing ICons
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdSnow,
+  IoMdThunderstorm,
+} from "react-icons/io";
 
 const App = () => {
   return <div>react app</div>;
